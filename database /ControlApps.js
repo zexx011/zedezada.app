01cb5e03cebf8b953500e2af36854c6766abf76f
zexx011 <zyexxyz@gmail.com>
2026-01-12 19:59:05 +0700

    Create ControlApps.js with configuration settings
    Add configuration for ControlApps module

---
+++ database /ControlApps.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8446335641:AAFXpeqtvoRLedL3PVj_0DrpgszTFQCYR_o", 
+  owner: "8482486283", 
+  port: "2291", // Ini Wajib Jangan Diubah
+  ipvps: "http://rizz.myserverku.cloud" // Jangan Diubah Nanti Eror!!
+};
